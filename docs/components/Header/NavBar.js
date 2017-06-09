FirePrometheus0109/phenomic

commit 60b26902a51995975850e88b412788de93661625

Fix wrongly spelled CSS property in docs

docs/components/Header/NavBar.js
@@ -73,7 +73,7 @@ const styles = StyleSheet.create({
     alignItems: "baseline",
     justifyContent: "center",
     paddingVertical: 10,
-    flewGrow: 1
+    flexGrow: 1
   },
   text: {
     color: "#fff",

commit 0745ad00eceecd10c0c26319e63fa8e626eaf3ae

docs: fix regression introduced in 60b26902a51995975850e88b412788de93661625

docs/components/Header/NavBar.js
@@ -7,7 +7,7 @@ import { version } from "../../../lerna.json";
 
 const HeaderNavBar = () => (
   <View style={styles.header}>
-    <View style={styles.row}>
+    <View style={styles.logo}>
       <Link to="/" style={styles.text}>
         <img src="/assets/phenomic-logo-baseline.svg" height="48" />
       </Link>
@@ -68,12 +68,11 @@ const styles = StyleSheet.create({
     paddingHorizontal: 20,
     paddingVertical: 10
   },
-  row: {
+  logo: {
     flexDirection: "row",
     alignItems: "baseline",
     justifyContent: "center",
-    paddingVertical: 10,
-    flexGrow: 1
+    paddingVertical: 10
   },
   text: {
     color: "#fff",